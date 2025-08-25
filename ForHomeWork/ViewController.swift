dd3c3edb31b842df724b9840ff501c60e4b9d8cb
Kaimov-i <117822275+Kaimov-i@users.noreply.github.com>
2025-08-25 07:02:17 +0300

    home work, lesson 16

--- ForHomeWork/ViewController.swift
+++ ForHomeWork/ViewController.swift
@@ -11,37 +11,58 @@ class ViewController: UIViewController {
 
     private let helper = Helper()
     private let userRepository = UserRepository()
-    private let textLabel = UILabel()
     
-    private var button: UIButton = {
-        let button = UIButton()
-        button.setTitle("Show FullName", for: .normal)
-        button.backgroundColor = .clear
-        button.frame = CGRect(x: 100, y: 150, width: 150, height: 50)
-        return button
-    }()
+    private let textLabel = UILabel()
+    private let button = UIButton()
+    private let stakView = UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .brown
         helper.addpersons(userRepository.getAllUsers())
         helper.getPerson().forEach { user in
             print(user)
         }
         
-        view.addSubview(button)
+        setupButton()
         setupLabel()
-        view.addSubview(textLabel)
-        
+        setupStackView()
+        view.addSubview(stakView)
+        setupConstraints()
     }
     
     private func setupLabel() {
         guard let randomUser = helper.getPerson().randomElement()?.userName else { return }
         textLabel.text = randomUser
         textLabel.font = .systemFont(ofSize: 25)
+        textLabel.textAlignment = .center
         textLabel.textColor = .blue
         textLabel.frame = CGRect(x: 100, y: 100, width: 100, height: 50)
     }
     
+    private func setupButton()  {
+        button.setTitle("Show FullName", for: .normal)
+        button.backgroundColor = .clear
+        button.frame = CGRect(x: 100, y: 150, width: 150, height: 50)
+    }
+    
+    private func setupStackView() {
+        stakView.axis = .vertical
+        stakView.distribution = .fill
+        stakView.spacing = 20
+        stakView.addArrangedSubview(textLabel)
+        stakView.addArrangedSubview(button)
+    }
+    
+    private func setupConstraints() {
+        stakView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stakView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            stakView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            stakView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
+        ])
+    }
 }
 
